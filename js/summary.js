9029029db5c153d1a1622bbd3645eb385e774b22
abdussamadsaiful7 <samadsaifullah11@gmail.com>
2023-03-15 02:14:48 +0600

    solve search button

---
+++ js/summary.js
@@ -0,0 +1,42 @@
+/* 
+FETCH
+1. Fetch must provide url
+dynamic or static
+2. how to convert fetch promise return to json 
+3. how to convert json to data
+4. [cool headed]: 
+users: 
+--> array of objects
+--> object with property users
+---> object with a property called data
+
+nested object
+
+
+---------------
+DOM manipulation
+1. get something from the DOM
+2. create element to append to the DOM
+3. dynamically load data based on id 
+
+----------------
+array --> forEach, map, find, filter
+------------------------
+template string
+
+
+
+Search button not working -(done)
+after typing something on the search textBox, you should be able to press enter to get search result
+spinner is always running. should be displayed at the time of data loading
+If you search for 'iphone', check the api, you should get a lot of data but the UI displays only one phone
+Search is not clearing previous results
+picture of the phones are not showing
+Show All button should be hidden after all data is loaded
+Load phone details doesn't display anything
+Phone details are added on the modal but this is not getting displayed. why?
+Storage on Modal isn't getting displayed properly
+
+
+
+*/
